9b8d4e95e148f200f0e86f9d73e5f792db2b63a1
Matteo Fernandes <mfernand@f1br8s20.clusters.42paris.fr>
2025-05-19 02:04:38 +0200

    only double and norm

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mfernand <mfernand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 15:32:44 by mfernand          #+#    #+#             */
-/*   Updated: 2025/05/19 01:34:19 by mfernand         ###   ########.fr       */
+/*   Updated: 2025/05/19 02:03:35 by mfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,59 +43,15 @@
 // 	return (0);
 // }
 
-// #include "push_swap.h"
-
-// int	main(int ac, char **av)
-// {
-// 	t_stack_node *a = NULL;
-// 	t_stack_node *b = NULL;
-// 	char **args = av + 1;
-// 	int split_mode = 0;
-
-// 	split_mode = 0
-// 	if (ac < 2)
-// 		return (0);
-// 	if (ac == 2 && !av[2])
-// 	{
-// 		args = ft_split(av[1], ' ');
-// 		if (!args)
-// 			return (1);
-// 		split_mode = 1;
-// 	}
-// 	if (!ft_stack_copy(&a, args))
-// 	{
-// 		if (split_mode)
-// 			free_tab(args);
-// 		return (0);
-// 	}
-// 	if (!a || is_sorted(a))
-// 	{
-// 		if (split_mode)
-// 			free_tab(args);
-// 		free_stack(&a);
-// 		return (0);
-// 	}
-// 	if (ft_stacksize(a) <= 3)
-// 		sort_three(&a);
-// 	else
-// 		ft_sort_stack(&a, &b);
-// 	free_stack(&a);
-// 	if (split_mode)
-// 		free_tab(args);
-// 	return (0);
-// }
+#include "push_swap.h"
 
 int	main(int ac, char **av)
 {
-	t_stack_node	*a;
-	t_stack_node	*b;
-	char			**args;
-	int				split_mode;
+	t_stack_node *a = NULL;
+	t_stack_node *b = NULL;
+	char **args = av + 1;
+	int split_mode = 0;
 
-	a = NULL;
-	b = NULL;
-	args = av + 1;
-	split_mode = 0;
 	if (ac < 2)
 		return (0);
 	if (ac == 2 && !av[2])
@@ -127,3 +83,46 @@ int	main(int ac, char **av)
 		free_tab(args);
 	return (0);
 }
+
+// int	main(int ac, char **av)
+// {
+// 	t_stack_node	*a;
+// 	t_stack_node	*b;
+// 	char			**args;
+// 	int				split_mode;
+
+// 	a = NULL;
+// 	b = NULL;
+// 	args = av + 1;
+// 	split_mode = 0;
+// 	if (ac < 2)
+// 		return (0);
+// 	if (ac == 2 && !av[2])
+// 	{
+// 		args = ft_split(av[1], ' ');
+// 		if (!args)
+// 			return (1);
+// 		split_mode = 1;
+// 	}
+// 	if (!ft_stack_copy(&a, args))
+// 	{
+// 		if (split_mode)
+// 			free_tab(args);
+// 		return (0);
+// 	}
+// 	if (!a || is_sorted(a))
+// 	{
+// 		if (split_mode)
+// 			free_tab(args);
+// 		free_stack(&a);
+// 		return (0);
+// 	}
+// 	if (ft_stacksize(a) <= 3)
+// 		sort_three(&a);
+// 	else
+// 		ft_sort_stack(&a, &b);
+// 	free_stack(&a);
+// 	if (split_mode)
+// 		free_tab(args);
+// 	return (0);
+// }
